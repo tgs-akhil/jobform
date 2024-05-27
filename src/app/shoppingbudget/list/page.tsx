b364303068a3eafcce69f63491df2108aae527c8
tgs-akhil <akhil@thegeeksquad.tech>
2024-05-27 18:36:49 +0530

    fixed database tables

--- src/app/shoppingbudget/list/page.tsx
+++ src/app/shoppingbudget/list/page.tsx
@@ -1,16 +1,21 @@
 import { db } from "@/src/db";
+
 export default async function RenderList() {
-  const list = await db.Shopping.findMany();
+  const list = await db.shopping.findMany();
 
-  return JSON.stringify(list);
+  return JSON.stringify(list, (key, value) =>
+    typeof value === "bigint" ? value.toString() : value
+  );
 
-  // const renderList = list.map((item) => {
-  //   return (
-  // <div key={item.id}>
-  //   {item.date} = {item.budget}
-  // </div>
-  //   );
-  // });
-
-  // return <div>{renderList}</div>;
+  // If rendering the list, convert BigInt to string to avoid issues with React rendering
+  // return (
+  //   <div>
+  //     {list.map((item) => (
+  //       <div key={item.id}>
+  //         <p>Date: {item.date}</p>
+  //         <p>Budget: {item.budget.toString()}</p>
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
 }
